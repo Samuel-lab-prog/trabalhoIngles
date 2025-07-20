a752aefe91d52f2f0679c43d5c45d70181a6c373
Samuel <sg1976387@gmail.com>
2025-07-20 12:34:33 -0300

    Version 1.0

--- script.js
+++ script.js
@@ -4,69 +4,84 @@ const selectFood = document.getElementById("foodOptions");
 const creatureImage = document.querySelector("#creatureImage img");
 const happinessLevel = document.querySelector("#happinessLevel");
 const healthLevel = document.querySelector("#healthLevel");
-let type = null;
 
+let type = 'Healthy';
+let character = "Hello Kitty";
 let happinessLevelValue = 50;
 let healthLevelValue = 50;
 let value = 1;
 
-function updateCreatureImage(value) {
-    creatureImage.style.setProperty("--size", value);
+function updateCreatureImage(size) {
+  creatureImage.style.setProperty("--size", size);
 }
+
 function changeCreatureImage(character) {
   creatureImage.src = `images/${character}.webp`;
-  happinessLevelValue = 50;
-  healthLevelValue = 50;
-  value = 1;
+  value = 1; 
+  happinessLevelValue = 50; 
+  healthLevelValue = 50; 
+  updateCreatureImage(value);
+  updateHappinessLevel(happinessLevelValue);
+  updateHealthLevel(healthLevelValue);
 }
+
 function updateHappinessLevel(level) {
   happinessLevel.style.setProperty("--happiness-level", `${level}%`);
 }
+
 function updateHealthLevel(level) {
   healthLevel.style.setProperty("--health-level", `${level}%`);
 }
 
+updateCreatureImage(value);
+updateHappinessLevel(happinessLevelValue);
+updateHealthLevel(healthLevelValue);
+
 selectCharacter.addEventListener("change", (event) => {
   selectedCharacter = event.target.value;
+  character = selectedCharacter;
   changeCreatureImage(selectedCharacter.replace(/\s+/g, ""));
 });
+
 selectFood.addEventListener("change", (event) => {
-  let optionSelected = event.target.selectedOptions[0];
+  let optionSelected = event.target.options[event.target.selectedIndex];
   if (optionSelected.parentElement.label === "healthy") {
     type = "Healthy";
   } else {
     type = "Unhealthy";
   }
 });
+
 feedButton.addEventListener("click", () => {
-  if (type == "Healthy") {
-    if (happinessLevelValue - 10 < 0) {
-      alert("Your creature is too sad to eat healthy food!");
+  if (type === "Healthy") {
+    if (happinessLevelValue + 10 > 100) {
+      alert(`${character} is too sad to eat healthy food!`);
       return;
-    } else {
-        if(!(value - 1 < 1)){
-            value -= 1;
-            updateCreatureImage(value);
-        }
-      happinessLevelValue -= 10;
-      healthLevelValue += 10;
-      updateHappinessLevel(happinessLevelValue);
-      updateHealthLevel(healthLevelValue);
     }
-  } else {
-    if (healthLevelValue - 10 < 0) {
-      alert("Your creature is too unhealthy to eat unhealthy food!");
-      return;
-    } else {
-      if(!(happinessLevelValue >= 50 )){
-            value += 1;
-            updateCreatureImage(value);
-        }
-      happinessLevelValue += 10;
-      healthLevelValue -= 10;
+
+    if (value > 1) {
+      value -= 1;
       updateCreatureImage(value);
-      updateHappinessLevel(happinessLevelValue);
-      updateHealthLevel(healthLevelValue);
     }
+
+    happinessLevelValue += 10;
+    healthLevelValue -= 10;
+
+  } else if (type === "Unhealthy") {
+    if (healthLevelValue + 10 > 100) {
+      alert(`${character} is too unhealthy to eat unhealthy food!`);
+      return;
+    }
+
+    if (happinessLevelValue <= 50) {
+      value += 1;
+      updateCreatureImage(value);
+    }
+
+    happinessLevelValue -= 10;
+    healthLevelValue += 10;
   }
+
+  updateHappinessLevel(happinessLevelValue);
+  updateHealthLevel(healthLevelValue);
 });
